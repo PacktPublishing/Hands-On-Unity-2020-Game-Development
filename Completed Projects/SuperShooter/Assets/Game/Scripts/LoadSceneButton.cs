952e9ecb91274b9e5efd563f110cbc7101579882
borro56 <nicolas.borromeo@gmail.com>
2020-07-10 15:09:21 +0100

    Try Again Buttons

---
+++ Completed Projects/SuperShooter/Assets/Game/Scripts/LoadSceneButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneButton : MonoBehaviour
+{
+    public string sceneName;
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
